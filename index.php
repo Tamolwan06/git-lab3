46c592c37446680c766c6c05607688b556e3d427
Tamolwan06 <6611130006@mut.ac.th>
2025-07-17 16:41:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     echo "This subject is Git Tutorail";
     echo "This subject is Git Tutorail!!";
 
-    echo "This subject is Git Tutorail!!";
-    echo "This subject is Git Tutorail!!";
+    echo "add file เพิ่มเติมแล้วครับ";
+    echo "แก้บัคและErorr เรียบร้อย";
+
 ?>
